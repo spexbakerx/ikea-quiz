a37523c6087d70dc7ba0bcd7c8dda5ff002453f1
Lauren Baker <spexbakerx@gmail.com>
2018-02-15 00:57:24 -0800

    having troubles with checking answers

--- index-2.js
+++ index-2.js
@@ -1,60 +1,71 @@
-(function() {
+
+
+let currentQuestion= 0;
+let correctTotal= 0;
+
+// Array of Objects that are the questions and answers
+(function() { 
   const myQuestions = [
+
+    // Question #1
     {
-      id: 1,
       question: "How old was Ingvar Kamprad (R.I.P.) when he founded IKEA in 1943?",
-      answers: {
-        a: "24",
-        b: "17",
-        c: "89"
-      },
-      correctAnswer: "b",
+      answers: [
+        24,
+        17,
+        89
+      ],
+      correctAnswer: 17,
       explanation: "He was also an admitted member of the Nazi party back in the day, so maybe not R.I.P.?"
     },
+
+    // Question #2
     {
-      id: 2,
       question: "What does IKEA stand for?",
-      answers: {
-        a: "International Klein Everything Always",
-        b: "I Kan't Even Afford (this)",
-        c: "Ingvar Kamprad Elmtaryd Agunnaryd",
-        d: "Who cares it looks cool "
-      },
-      correctAnswer: "c",
+      answers: [
+        "International Klein Everything Always",
+        "I Kan't Even Afford (this)",
+        "Ingvar Kamprad Elmtaryd Agunnaryd",
+        "Who cares it looks cool "
+      ],
+      correctAnswer: "Ingvar Kamprad Elmtaryd Agunnaryd",
       explanation: "The first two letters are the founder's initials, Elmtaryd and Agunnaryd are the farm and village he grew up in, respectively."
     },    
+
+    // Question #3
     {
-      id: 3,
       question: "IKEA uses approximately how much of the world's entire commercial supply of wood?",
-      answers: {
-        a: "1%",
-        b: "13%",
-        c: "100%"
-      },
-      correctAnswer: "a",
+      answers: [
+        "1%",
+        "13%",
+        "100%"
+      ],
+      correctAnswer: "1%",
       explanation: "That's a lot of dang wood."
     },
+
+    // Question #4
     {
-      id: 4,
       question: "Ikea was the first company to feature BLANK in one of its ads",
-      answers: {
-        a: "A talking dog (eat shit, Air Bud)",
-        b: "A gay couple (eat shit, straight people)",
-        c: "Another company's logo (way to go, Target)",
-        d: "JNCO Jeans"
-      },
-      correctAnswer: "b",
+      answers: [
+        "A talking dog (eat shit, Air Bud)",
+        "A gay couple (eat shit, straight people)",
+        "Another company's logo (way to go, Target)",
+        "JNCO Jeans"
+      ],
+      correctAnswer: "A gay couple (eat shit, straight people)",
       explanation: ":') thx IKEA"
     },
+
+    // Question #5
     {
-      id: 5,
       question: "Ikea changed it's typeface from Futura to Verdana in what year?",
-      answers: {
-        a: "2000",
-        b: "2017",
-        c: "1995",
-        d: "2009",
-      },
+      answers: [
+      "2000",
+      "2017",
+      "1995",
+      "2009",
+      ],
       correctAnswer: "d",
       explanation: "Understandably, this caused a bit of uproar on the interwebs"
     }  
@@ -75,7 +86,7 @@
         // ...add an HTML radio button
         answers.push(
           `<label>
-             <input type="radio" name="question${questionNumber}" value="${letter}">
+             <input type="radio" name="question${questionNumber}" value="${currentQuestion.answers[letter]}">
               ${currentQuestion.answers[letter]}
            </label>`
         );
@@ -94,33 +105,99 @@
     quizContainer.innerHTML = output.join("");
   }
 
-  function showResults() {
 
 
-    var currentQuestion = myQuestions.indexOf();
-    console.log(currentQuestion);
-    // myQuestions.forEach(function(currentQuestion) {
-    // console.log(currentQuestion.correctAnswer);
-    // });
+//Function to handle answers,update counter and score
+function handleAnswer(){
+  $("#quiz").on('click', '#check', function (event){
+    event.preventDefault();    
+    let choice = $('input:checked');
+    let userAnswer = choice.val();
+    console.log(userAnswer);
+    checkAnswer(userAnswer);
 
+    updateQuestion();
+    nextButton.style.display = "none";
+    submitButton.style.display = "none";
+    checkButton.style.display = "inline-block";
+    
+  });
+  
+}
 
+//Function to check for correct/incorrect answer
+function checkAnswer(answer){
 
+    let rightAnswer = `${myQuestions[currentQuestion].correctAnswer}`;
 
-      // if answer is correct
-      if (currentQuestion.correctAnswer.is(':checked')) {
-
-        // show correct and explanation
-        $('#explanation').html('<strong>Correct!</strong> ' + htmlEncode(quiz[currentQuestion]['explanation']));
-      } 
-      else {
-        // if answer is wrong or blank
-        // color the answers red
-        answerContainers[questionNumber].style.color = "red";
-
-        // show incorrect and explanation
-        $('#explanation').html('<strong>Incorrect.</strong> ' + htmlEncode(quiz[currentQuestion]['explanation']));
-      };
+    if (answer === rightAnswer) {
+      console.log("Great Job. That's correct.");
+      $("#quiz").addClass("hidden");
+      $(".quiz-container").html(displayCorrectAnswerMessage());
+      updateScore();
     }
+    else{
+      console.log("I'm sorry. That's not correct.");
+      $("#quiz").addClass("hidden");
+      $(".quiz-container").html(displayWrongAnswerMessage());
+    } 
+
+    nextButton.style.display = "inline-block";
+    submitButton.style.display = "none";
+    checkButton.style.display = "none";
+}
+
+//Function to display correct answer 
+function displayCorrectAnswerMessage(){
+  return `<section class="feedback-page popup">
+            <h1> Nice job! That's correct. </h1>
+            <p> ${myQuestions[currentQuestion].explanation} </p>
+//         <button class= "nextButton" value="next"> Next Question</button>
+          </section>`;
+  }
+
+//Function to display Wrong answer
+function displayWrongAnswerMessage(){
+  let displayAnswer= `${myQuestions[currentQuestion].correctAnswer}`;
+  return `<section class="feedback-page popup">
+            <h1> Nope - the correct answer is ${displayAnswer}. </h1>
+            <p> ${myQuestions[currentQuestion].explanation} </p>
+ 
+          </section>`;
+  }
+
+//Function to update score and question #
+function updateScore(){
+  correctTotal ++;
+  console.log('updateScore ran');
+}
+
+
+//Function to move to next question
+function nextQuestion(){
+  $('.quiz-container').on ('click', '#next' ,function(event){
+   event.preventDefault();
+   console.log('nextQuestion ran');
+   
+  if (currentQuestion < 6){
+    buildQuiz();
+    handleAnswer();
+  }
+  else {
+    endResults();
+  }
+  
+  })
+}
+
+
+//Function to get next question
+function updateQuestion(){
+  currentQuestion++;
+  console.log(currentQuestion);
+}
+
+
 
 
 
@@ -172,6 +249,8 @@
     }
   }
 
+
+
   function showNextSlide() {
     showSlide(currentSlide + 1);
   }
@@ -197,7 +276,7 @@
 
   // on submit, show results
   submitButton.addEventListener("click", endResults);
-  checkButton.addEventListener("click", showResults);
+  checkButton.addEventListener("click", handleAnswer);
   previousButton.addEventListener("click", showPreviousSlide);
   nextButton.addEventListener("click", showNextSlide);
 })();
